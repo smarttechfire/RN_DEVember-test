3f9fc555494caba5e5702c8d246a3e59615dc428
smarttechfire <educationfire552@gmail.com>
2024-01-28 00:24:12 +0530

    ts paths

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { ActivityIndicator, FlatList, StyleSheet, Text, View } from 'react-native';
-import DayListItem from '../../src/components/core/DayListItem';
+import DayListItem from '@components/core/DayListItem';
 
 
 
